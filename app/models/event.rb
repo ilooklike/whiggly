19071a9b1ccfdc9ea81def45f789ae82505aedf8
Yanran Lu <luyanran@yahoo.com>
2014-12-01 17:11:25 -0800

    add search icon

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,8 @@
 class Event < ActiveRecord::Base
   validates :title, :description, :start_time, :end_time, :category, 
             :longtitude, :latitude, presence: true
+  
+  def self.search(params)
+    
+  end
 end
