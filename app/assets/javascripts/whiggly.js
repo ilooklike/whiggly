2dc21319c3fb290df058c1a3c8aa750e094bd030
Yanran Lu <luyanran@yahoo.com>
2014-12-02 22:56:11 -0800

    expandable search button

--- app/assets/javascripts/whiggly.js
+++ app/assets/javascripts/whiggly.js
@@ -12,7 +12,13 @@ window.Whiggly = {
 			event.preventDefault();
 			var query = $(event.currentTarget).serialize();
 			Backbone.history.navigate("?" + query, { trigger: true })
-		})
+		});
+		// $("#search-button").hover(function(event) {
+// 			$(".form-group").removeClass("hide-form");
+// 			$(".form-group").addClass("active");
+// 			$(".form-group input:first").click();
+// 			// $(".form-group input:first").addClass("active")
+// 		})
 		// function mapInit() {
 		//   var mapOptions = {
 		//     center: { lat: -34.397, lng: 150.644},
